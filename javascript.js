ced3bf7cde8124bf0e4a1137451dd3a9959bf0a2
boriyevv08 <manoken999@gmail.com>
2022-09-27 16:11:36 +0500

    JavaScript

---
+++ javascript.js
@@ -0,0 +1,226 @@
+"use strict"
+            // M 1
+// let x=+prompt("L uzunligini kiriting:");
+// console.log(`${x} sm ---> ${x/100} metr`);
+// document.write(`${x} sm = ${x/100} metr`);
+
+            // M 2
+//let x=+prompt("M og'irlikni kiriting:");
+// console.log(`${x} kg ---> ${x/1000} tonna`); 
+// document.write(`${x} kg = ${x/1000} tonna`);
+
+
+            // M 3
+// let A=+prompt("A ni kiriting"),
+// B=+prompt("B nikiriting");
+// if(A>B){
+//    console.log(`A ni B ga ${A/B} marta joylashtirish mumkin`);
+//    document.write (`A ni B ga ${A/B} marta joylashtirish mumkin`);
+// }
+
+// else{
+//    console.log("nimadir xato ketdi, qaytadan kiriting");
+//    document.write("nimadir xato ketdi, qaytadan kiriting");
+// }
+
+         // M 4
+
+// let x=+prompt("Ikki xonali son kiriting");
+// console.log(`O'nlar xonasidagi son ${x/10} birlar xonasidagi son ${x%10}`);    
+// document.write(`O'nlar xonasidagi son ${x/10} birlar xonasidagi son ${x%10}`); 
+
+         //  M 5
+
+// let x=+prompt("ikki xoali son kiriting")
+// console.log(`${Math.trunc(x/10) + (x%10)}`);
+// document.write(`${Math.trunc(x/10)+(x%10)}`);
+
+
+          // M 6 
+
+//  let x=+prompt("ikki xoali son kiriting");
+//  console.log(`${Math.trunc(x/10)*(x%10)}`);
+//  document.write(`${Math.trunc(x/10)*(x%10)}`);
+
+         // M 7
+
+// let x=+prompt( "999 dan katta son kiriting");
+// console.log ( `${(x%999)/100}`)
+// document.write ( `${(x%999)/100}`)
+
+      //  M 8
+
+// let x=+prompt("Kun boshidan beri o'tgan taxminiy N sekundni kiriting");
+// console.log(`Kun bsohidan ${x/60} minut va ${x/3600}soat o'tdi`);
+// document.write(`Kun bsohidan ${x/60} minut va ${x/3600}soat o'tdi`);
+ 
+     //   M 9
+
+// let a=+prompt("A=?")
+// if (a>0){
+//    console.log(`${a} soni musbat`);
+// }
+// else{
+//    console.log(`${a} soni manfiy`);
+// }
+//   document.write( `${a>0} ${a}`);
+
+
+      //   M 10
+
+// let a=+prompt ("A=?");
+// if (a%2==1){
+//    console.log(`${a} soni toq`)
+// }      
+
+// else {
+//    console.log(`${a} soni juft`)
+// }
+
+// document.write(`${a%2==1} ${a}`)
+
+      //  M11
+
+// let a=+prompt ("A=?");
+// if (a%2==0){
+//    console.log(`${a} soni juft`)
+// }      
+
+// else {
+//    console.log(`${a} soni toq`)
+// }
+
+// document.write(`${a%2==1} ${a}`)
+
+
+      //  M 12
+
+// let a=+prompt("A=?"), b=+prompt("B=?");       
+// if (a%1==0&&b%1==0){
+//       console.log (`${a} va ${b} sonlari butun sonlar`)
+// }
+
+// else {
+//       console.log(`${a} va ${b} sonlari butun emas`)
+// }
+
+// document.write(`${a%1==0 && b%1==0} `)
+
+
+    //  M 13
+
+// let a=+prompt("A=?"), b=+prompt("B=?"), c=+prompt("C=?")
+// if (a > b && b > c) {
+//       console.log(`${a} >${b} >${c}`)
+// }
+
+// else {
+//       console.log(`${a} <${b} <${c}`)  
+// }
+
+
+//      M 14 
+
+// let a=+prompt("A=?"),b=+prompt("B=?");
+// if(a%2==1 && b%2==1){
+//    console.log(`${a} va ${b} sonlari ikkisi xam toq son `);
+// }
+
+// else if(a%2==1 && b%2==0){
+//    console.log(`a=${a} soni toq son `);
+// }
+// else if(a%2==0 && b%2==1){
+//    console.log(`b=${b} soni toq son `);
+// }
+
+// else{
+//    console.log(`Ikkisi xam juft son`);
+// }
+
+
+            //  M 15 
+
+// let a=+prompt("A=?"),b=+prompt("B=?"), c=+prompt("C=?"),d=+prompt("D=?");
+// if(a>0 && b>0 && c>0 && d>0){
+//    console.log(`${a} ,${b} ,${c}, ${d}  musbat sonlar `);
+// }
+
+// else{
+//    console.log(`xato`);
+// }
+
+//         M 16 
+// let a = +prompt("A=?"), b = +prompt("B=?"), c = +prompt("C=?");
+// if ((a > 0 && b > 0) || (a > 0 && c > 0) || (b > 0 && c > 0)) { console.log(" ikkitasi Musbat "); }
+// else {
+//    console.log(`xato`);
+// }
+
+
+//        M 17
+
+// let HaftaKunlari ={1:"Dushanba",2:"Seshanba",3:"chorshanba",4:"Payshanba",5:"Juma",6:"Shanba",7:"Yakshanba"};
+// let kun=+prompt("xafta kuni raqami:")
+// console.log(HaftaKunlari[kun]);
+
+//         M 18
+// let telfonCodlari={99893:"Ucell",99899:"Uzmobile",99890:"Bilene",99897:"UMC",99895:"Damashniy"};
+// let code=+prompt("Telefon codi:")
+// console.log(telfonCodlari[code]);
+
+
+//        M 19
+// let number=+prompt("Number ni kiriting ?")
+// if(number>0){
+//    console.log(`son ${number} musmat 1 taga ga oshirildi ----> ${number+1}`);
+// }
+// else{
+//    console.log(`son ${number} manfiy  -----> ${number-1}`);
+// }
+
+
+//        M 20
+
+// let number=+prompt("Number ni kiriting ?")
+// if(number>0){
+//    console.log(`son ${number} musmat 3  ga oshirildi ----> ${number+3}`);
+// }
+// else{
+//    console.log(`son ${number} manfiy  -----> ${number-2}`);
+// }
+
+
+//        M 21
+
+
+// let x=+prompt("x ni kiriting ?"), y=+prompt("y ni kiriting ?");
+// if(x>y){
+//    console.log(`x=${x} katta`);
+// }
+// else{
+//    console.log(`y=${y} katta`);
+// }
+
+
+
+//        M 22
+
+// let x=+prompt("x ni kiriting ?"), y=+prompt("y ni kiriting ?"),z=+prompt("z ni kiriting ?");
+// if(x>y && x>z){
+//    console.log(`x=${x} katta`);
+// }
+// else if (x<y && y>z){
+//    console.log(`x=${y} katta`);
+// }
+
+// else{
+//    console.log(`z=${z} katta`);
+// }
+
+
+
+//       M 23
+
+
+// let x=+prompt("x ni kiriting ?"), y=+prompt("y ni kiriting ?");
+// console.log(Math.min(x,y,z));
